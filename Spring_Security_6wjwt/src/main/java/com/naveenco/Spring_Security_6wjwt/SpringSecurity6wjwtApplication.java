627cfb12cf83e2b5ea5b858eb5784674f3f1d389
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-06-23 09:38:00 +0530

    Project set

---
+++ Spring_Security_6wjwt/src/main/java/com/naveenco/Spring_Security_6wjwt/SpringSecurity6wjwtApplication.java
@@ -0,0 +1,13 @@
+package com.naveenco.Spring_Security_6wjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6wjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6wjwtApplication.class, args);
+	}
+
+}
