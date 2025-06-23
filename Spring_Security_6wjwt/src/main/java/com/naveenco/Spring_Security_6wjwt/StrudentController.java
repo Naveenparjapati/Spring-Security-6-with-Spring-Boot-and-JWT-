ebafb52585bbb539af0701c8313000d19c4917eb
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-06-23 10:44:31 +0530

    generating a token

--- Spring_Security_6wjwt/src/main/java/com/naveenco/Spring_Security_6wjwt/StrudentController.java
+++ Spring_Security_6wjwt/src/main/java/com/naveenco/Spring_Security_6wjwt/StrudentController.java
@@ -3,8 +3,13 @@ package com.naveenco.Spring_Security_6wjwt;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class StrudentController {
@@ -20,5 +25,24 @@ public class StrudentController {
 	public List<Student>  getStudent()
 	{
 		return students;
+	}
+	
+	//web csrf
+	@GetMapping("/csrf-token")
+	public CsrfToken getCsrfToken(HttpServletRequest request)
+	{
+		return (CsrfToken) request.getAttribute("_csrf");
+	}
+	{
+		
+	}
+	
+	
+	//we are adding student
+	@PostMapping("/students")
+	public Student addStudent(@RequestBody Student student)
+	{
+		students.add(student);
+		return student;
 	}
 }  
