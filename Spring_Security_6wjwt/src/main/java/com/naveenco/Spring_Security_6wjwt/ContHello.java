2f9400e578ebd65b936255f940f485a36edded30
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-06-23 09:55:48 +0530

    session id printing

--- Spring_Security_6wjwt/src/main/java/com/naveenco/Spring_Security_6wjwt/ContHello.java
+++ Spring_Security_6wjwt/src/main/java/com/naveenco/Spring_Security_6wjwt/ContHello.java
@@ -3,12 +3,14 @@ package com.naveenco.Spring_Security_6wjwt;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class ContHello {
 	
-	@RequestMapping("/")
-    public String greet() {
-        return "Hello World, Welcome to Telusko";
+	@RequestMapping("/")     //it has 2 object req or res 
+    public String greet(HttpServletRequest request) {
+        return "Hello World, Welcome to naveenco    "+request.getSession().getId();
     }
 
 }
